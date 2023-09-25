87ee0015593c9cffd404dd5d17d0f257a560c1ff
Wemimo Ibidunmoye <wemiibidun@gmail.com>
2023-09-25 17:27:03 -0600

    checked for correct and incorrect

--- js/script.js
+++ js/script.js
@@ -38,26 +38,33 @@ function checkinput() {
 }
 // this funtion checks the operator and tells a user if answer is correct or incorrect
 function guessResult() {
+  let answer;
+  let right_response = "Your answer is correct. You win!";
+  let wrong_response = "Your answer is incorrect. Try again!";
+  let users_result = parseInt(document.getElementById("users_result").value);
+
   if (operator == "+") {
-    let answer = first_number + second_number;
-    document.getElementById("display_answer").innerHTML = answer;
-    console.log("this is the add result");
-    console.log(answer);
-    return answer;
+    answer = first_number + second_number;
+    // document.getElementById("display_answer").innerHTML = answer;
+    if (users_result == answer) {
+      document.getElementById("display_answer").innerHTML = right_response;
+    } else {
+      document.getElementById("display_answer").innerHTML = wrong_response;
+    }
   } else if (operator == "-") {
-    let answer = first_number - second_number;
+    answer = first_number - second_number;
     document.getElementById("display_answer").innerHTML = answer;
     console.log("this is the subract result");
     console.log(answer);
     return answer;
   } else if (operator == "*") {
-    let answer = first_number * second_number;
+    answer = first_number * second_number;
     document.getElementById("display_answer").innerHTML = answer;
     console.log("this is the multiply result");
     console.log(answer);
     return answer;
   } else if (operator == "/") {
-    let answer = first_number / second_number;
+    answer = first_number / second_number;
     document.getElementById("display_answer").innerHTML = answer;
     console.log("this is the division result");
     console.log(answer);
@@ -66,8 +73,6 @@ function guessResult() {
 }
 
 result = guessResult();
-console.log("this is the result");
-console.log(result);
 
 // function displayCorrectOrIncorrect() {
 //  let result = guessResult();
